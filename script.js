e9569b33275551d3b0b41e3af556f56dbc112481
lovishverma1435 <vermalovey5@gmail.com>
2024-10-29 17:38:42 +0530

    sec commit

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 const calculator = document.getElementById(`calculator`)
 
-// function insertValue(value) {
-//     calculator.value += value;
-// }
+function insertValue(value) {
+    calculator.value += value;
+}
 
-// function resultClear() {
-//     calculator.value = ``;
-// }
+function resultClear() {
+    calculator.value = ``;
+}
 
-// function deleteResult() {
-//     calculator.value = calculator.value.slice(0 , -1);
-// }
+function deleteResult() {
+    calculator.value = calculator.value.slice(0 , -1);
+}
 
-// function calculate() {
-//     try{
-//         calculator.value = eval(calculator.value);
-//     }
-//     catch(error){
-//         calculator.value = "error";
-//     }
-// }
-
-
+function calculate() {
+    try{
+        calculator.value = eval(calculator.value);
+    }
+    catch(error){
+        calculator.value = "error";
+    }
+}
 
 
 
@@ -62,33 +60,34 @@ const calculator = document.getElementById(`calculator`)
 
 
 
-let calcu = "";
 
-const buttons = document.querySelectorAll(`#all_buttons .button`)
-buttons.forEach((button) => {
-    button.addEventListener(`click`, () => {
-        if (button.value == "=") {
-            calcu = eval(calcu)
-            calculator.value = calcu
-        }
-        else if (button.value === "AC") {
-            console.log("AC");
-            calculator.value = "";
+
+// let calcu = "";
+
+// const buttons = document.querySelectorAll(`#all_buttons .button`)
+// buttons.forEach((button) => {
+//     button.addEventListener(`click`, () => {
+//         if (button.value == "=") {
+//             calcu = eval(calcu)
+//             calculator.value = calcu
+//         }
+//         else if (button.value === "AC") {
+//             calculator.value = "";
             
-        }
+//         }
 
-        else if (button.value === "DEL") {
-            let l = calcu.length - 1
-            let bb = calcu.slice(0, l)
-            calcu = bb
-            calculator.value = calcu
-        }
-        else {
-            let value = button.value
-            calcu += value
-            calculator.value = calcu
-        }
+//         else if (button.value === "DEL") {
+//             let l = calcu.length - 1
+//             let bb = calcu.slice(0, l)
+//             calcu = bb
+//             calculator.value = calcu
+//         }
+//         else {
+//             let value = button.value
+//             calcu += value
+//             calculator.value = calcu
+//         }
         
 
-    })
-});     
+//     })
+// });     
