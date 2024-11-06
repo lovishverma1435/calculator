a16ac89c1476fffd3fae49b77247f16d596174fd
lovishverma1435 <vermalovey5@gmail.com>
2024-11-06 08:06:11 +0530

    rating

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 const calculator = document.getElementById(`calculator`)
 
-function insertValue(value) {
-    calculator.value += value;
-}
+// function insertValue(value) {
+//     calculator.value += value;
+// }
 
-function resultClear() {
-    calculator.value = ``;
-}
+// function resultClear() {
+//     calculator.value = ``;
+// }
 
-function deleteResult() {
-    calculator.value = calculator.value.slice(0 , -1);
-}
+// function deleteResult() {
+//     calculator.value = calculator.value.slice(0 , -1);
+// }
 
-function calculate() {
-    try{
-        calculator.value = eval(calculator.value);
-    }
-    catch(error){
-        calculator.value = "error";
-    }
-}
-
-
+// function calculate() {
+//     try{
+//         calculator.value = eval(calculator.value);
+//     }
+//     catch(error){
+//         calculator.value = "error";
+//     }
+// }
 
 
 
@@ -62,32 +60,35 @@ function calculate() {
 
 
 
-// let calcu = "";
+let calcu = "";
 
-// const buttons = document.querySelectorAll(`#all_buttons .button`)
-// buttons.forEach((button) => {
-//     button.addEventListener(`click`, () => {
-//         if (button.value == "=") {
-//             calcu = eval(calcu)
-//             calculator.value = calcu
-//         }
-//         else if (button.value === "AC") {
-//             calculator.value = "";
+const buttons = document.querySelectorAll(`.button`)
+buttons.forEach((button) => {
+    button.addEventListener(`click`, () => {
+        if (button.innerHTML== "=") {
+            calcu = eval(calcu)
+            calculator.value = calcu
+        }
+        else if (button.innerHTML === "AC") {
+            calcu = ""
+            calculator.value = calcu;
             
-//         }
+        }
 
-//         else if (button.value === "DEL") {
-//             let l = calcu.length - 1
-//             let bb = calcu.slice(0, l)
-//             calcu = bb
-//             calculator.value = calcu
-//         }
-//         else {
-//             let value = button.value
-//             calcu += value
-//             calculator.value = calcu
-//         }
+        else if (button.innerHTML === "DEL") {
+            let l = calcu.length - 1
+            let bb = calcu.slice(0, l)
+            calcu = bb
+            calculator.value = calcu
+        }
+        else {
+            
+            let value = button.innerHTML
+            calcu += value
+            console.log(calcu);
+            calculator.value = calcu
+        }
         
 
-//     })
-// });     
+    })
+});     
