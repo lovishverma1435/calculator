55cca920bce25adeba5eb2d6c795040ccbdc9924
lovishverma1435 <vermalovey5@gmail.com>
2025-08-25 10:28:05 +0530

    jfjdifj

--- script.js
+++ script.js
@@ -1,94 +1,21 @@
-const calculator = document.getElementById(`calculator`)
+const calculator = document.getElementById("calculator");
 
-// function insertValue(value) {
-//     calculator.value += value;
-// }
+function insertValue(value) {
+  calculator.value += value;
+}
 
-// function resultClear() {
-//     calculator.value = ``;
-// }
+function resultClear() {
+  calculator.value = "";
+}
 
-// function deleteResult() {
-//     calculator.value = calculator.value.slice(0 , -1);
-// }
+function deleteResult() {
+  calculator.value = calculator.value.slice(0, -1);
+}
 
-// function calculate() {
-//     try{
-//         calculator.value = eval(calculator.value);
-//     }
-//     catch(error){
-//         calculator.value = "error";
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let calcu = "";
-
-const buttons = document.querySelectorAll(`.button`)
-buttons.forEach((button) => {
-    button.addEventListener(`click`, () => {
-        if (button.innerHTML== "=") {
-            calcu = eval(calcu)
-            calculator.value = calcu
-        }
-        else if (button.innerHTML === "AC") {
-            calcu = ""
-            calculator.value = calcu;
-            
-        }
-
-        else if (button.innerHTML === "DEL") {
-            let l = calcu.length - 1
-            let bb = calcu.slice(0, l)
-            calcu = bb
-            calculator.value = calcu
-        }
-        else {
-            
-            let value = button.innerHTML
-            calcu += value
-            console.log(calcu);
-            calculator.value = calcu
-        }
-        
-
-    })
-});     
+function calculate() {
+  try {
+    calculator.value = eval(calculator.value);
+  } catch (error) {
+    calculator.value = "Error";
+  }
+}
